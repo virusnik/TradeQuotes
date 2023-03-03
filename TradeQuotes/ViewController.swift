618986cbb4f881b959e9dcb055f5c4b6da4aea92
Sergio Veliz <>
2023-03-03 16:47:01 +0300

    init project

---
+++ TradeQuotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TradeQuotes
+//
+//  Created by Sergio Veliz on 03.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
